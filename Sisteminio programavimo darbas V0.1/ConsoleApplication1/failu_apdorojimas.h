c699fe3e6c6932df001e4b5953d2f0878d3a5f1f
Arnas <arnas.sidorovicius@gmail.com>
2024-12-22 20:17:36 +0200

    Delete Sisteminio programavimo darbas V0.1/ConsoleApplication1 directory

--- Sisteminio programavimo darbas V0.1/ConsoleApplication1/failu_apdorojimas.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "studentas.h"
-#include <vector>
-#include <string>
-
-void generuotiAtsitiktiniusPažymius(std::vector<studentas>& studentai, int kiekis);
-void nuskaitytiIsFailo(const std::string& failoPavadinimas, std::vector<studentas>& studentai);
