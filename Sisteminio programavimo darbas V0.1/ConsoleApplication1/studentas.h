0a10dfd8106ad8a5fcdf61a77a27304e98ba51d1
Arnas <arnas.sidorovicius@gmail.com>
2024-12-22 19:54:00 +0200

    Add files via upload

---
+++ Sisteminio programavimo darbas V0.1/ConsoleApplication1/studentas.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+
+class studentas {
+private:
+    std::string vardas, pavarde;
+    std::vector<double> ndBalai; // Namų darbų balai
+    double egzaminas;
+    double galutinisVidurkis;
+    double galutinisMediana;
+
+public:
+    studentas();
+    studentas(std::string v, std::string p, std::vector<double> nd, double egzam);
+
+    void skaiciuotiVidurki();
+    void skaiciuotiMediana();
+    void spausdinti() const;
+
+    double getGalutinisVidurkis() const { return galutinisVidurkis; }
+    double getGalutinisMediana() const { return galutinisMediana; }
+
+    std::string getVardas() const { return vardas; }
+    std::string getPavarde() const { return pavarde; }
+};
